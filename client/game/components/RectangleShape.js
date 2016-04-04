e7fc7cee4e8c3a466275f5abc6a847b98a3e432e
domi <dominik.seifert@gmail.com>
2016-04-04 12:56:24 +0800

    - added RectangleShape component

--- client/game/components/RectangleShape.js
+++ client/game/components/RectangleShape.js
@@ -1,14 +1,8 @@
 'use strict';
 
-PhaserExtensions.defineComponent('Unit', {
+PhaserExtensions.defineComponent('RectangleShape', {
   init: function() {
     var body = this.gameObject.body;
-
-
-    // all units have P2 physics and cannot be moved by physics interactions
-    body.collideWorldBounds = true;
-    body.immovable = true;
-    body.fixedRotation = true;
 
     // add default collision shape
     body.setCircle(this.gameObject.width / 2);
